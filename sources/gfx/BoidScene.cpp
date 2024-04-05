83f29ef86eca54c2de3078cda736cfb6d65b8fbc
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-05 19:29:58 +0200

    deleted comments

--- sources/gfx/BoidScene.cpp
+++ sources/gfx/BoidScene.cpp
@@ -11,19 +11,12 @@ void BoidScene::update(const BoidContainer& container, TotoGL::Camera& camera) {
     }
     _boid_objects.clear();
     for (size_t i = 0; i < container.boids().size(); ++i) {
-        // auto& boid = container.boids()[i];
-        // auto& object = *_boid_objects[i];
-        // object.position() = boid.position();
-        // object.lookAt(boid.position() + boid.velocity());
-
         auto& boid = container.boids()[i];
         if (glm::distance(boid.position(), camera.position()) < 5) {
             _boid_objects.push_back(_boid_objects_high[i]);
         } else {
             _boid_objects.push_back(_boid_objects_low[i]);
         }
-        // _boid_objects[i]->position() = boid.position();
-        // _boid_objects[i]->lookAt(boid.position() + boid.velocity());
         _boid_objects_high[i]->position() = boid.position();
         _boid_objects_high[i]->lookAt(boid.position() + boid.velocity());
         _boid_objects_low[i]->position() = boid.position();
@@ -34,14 +27,9 @@ void BoidScene::update(const BoidContainer& container, TotoGL::Camera& camera) {
 void BoidScene::resize(const size_t& new_size) {
     const size_t old_size = _boid_objects.size();
     if (new_size > old_size) {
-        // _boid_objects.reserve(new_size);
         _boid_objects_high.reserve(new_size);
         _boid_objects_low.reserve(new_size);
         for (size_t i = old_size; i < new_size; ++i) {
-            // auto boid = _boid_object_base->clone();
-            // boid->scaling() = { .15, .15, .15 };
-            // _boid_objects.push_back(boid);
-
             auto boid_high = _boid_object_base_high->clone();
             boid_high->scaling() = { .15, .15, .15 };
             _boid_objects_high.push_back(boid_high);
@@ -52,11 +40,9 @@ void BoidScene::resize(const size_t& new_size) {
         }
     } else if (new_size < old_size) {
         for (size_t i = new_size; i < old_size; ++i) {
-            // TotoGL::MaterialObjectFactory::destroy(_boid_objects[i]);
             TotoGL::MaterialObjectFactory::destroy(_boid_objects_high[i]);
             TotoGL::MaterialObjectFactory::destroy(_boid_objects_low[i]);
         }
-        // _boid_objects.resize(new_size);
         _boid_objects_high.resize(new_size);
         _boid_objects_low.resize(new_size);
     }
