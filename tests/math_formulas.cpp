f2884c35f6d653aabafac24ecfb9383b3291a518
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-12 18:31:46 +0200

    moar unit tests

--- tests/math_formulas.cpp
+++ tests/math_formulas.cpp
@@ -30,7 +30,7 @@ TEST(Math, Binom) {
     }
 }
 
-// This is to explicitly avoid factorials for this
+// If this fails, that means you're probably using factorials. Please don't.
 TEST(Math, Binom_large) {
     // Generated with math.comb in Python
     auto units = std::map<uint8_t, uint64_t> {
@@ -42,4 +42,67 @@ TEST(Math, Binom_large) {
     for (auto& t : units) {
         EXPECT_EQ(Math::binom(t.first, t.first / 2), t.second);
     }
+}
+
+TEST(Math, Binomial_probability) {
+    // Generated with math.comb and ** in Python
+    auto units = std::map<std::tuple<float, uint8_t, uint8_t>, float> {
+        { { .5f, 4, 2 }, .375f },
+        { { .5f, 8, 4 }, .2734375f },
+        { { .5f, 12, 6 }, .2255859375f },
+        { { .5f, 16, 8 }, .196380615234375f },
+        { { .5f, 20, 10 }, .17619705200195312f }
+    };
+    const float ERR = 1e-9;
+    for (auto& t : units) {
+        auto& [p, n, k] = t.first;
+        EXPECT_NEAR(Math::binomialProbability(p, n, k), t.second, ERR);
+    }
+}
+
+template <class RNG>
+void test_rng(RNG& rng, int N, float expected, float variance, float err) {
+    double sum = 0;
+    double sum2 = 0;
+    for (int i = 0; i < N; i++) {
+        auto x = rng();
+        sum += x;
+        sum2 += x * x;
+    }
+    sum /= N;
+    sum2 /= N;
+    double var = sum2 - sum * sum;
+    EXPECT_NEAR(sum, expected, err);
+    EXPECT_NEAR(var, variance, err);
+}
+
+TEST(Random, Uniform) {
+    const int N = 1'000'000;
+    const float A = 0;
+    const float B = 1;
+    const float ERR = .01f;
+    auto rng = Random::Uniform<float>(A, B);
+    auto expected = (A + B) / 2;
+    auto variance = (B - A) * (B - A) / 12;
+    test_rng(rng, N, expected, variance, ERR);
+}
+
+TEST(Random, Exponential) {
+    const int N = 1'000'000;
+    const float P = 1;
+    const float ERR = .01f;
+    auto rng = Random::Exponential<float>(P);
+    auto expected = 1 / P;
+    auto variance = 1 / (P * P);
+    test_rng(rng, N, expected, variance, ERR);
+}
+
+TEST(Random, Poisson) {
+    const int N = 1'000'000;
+    const float L = 1;
+    const float ERR = .01f;
+    auto rng = Random::Poisson<float>(L);
+    auto expected = L;
+    auto variance = L;
+    test_rng(rng, N, expected, variance, ERR);
 }
