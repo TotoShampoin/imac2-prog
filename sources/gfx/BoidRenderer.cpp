35c4f57258dd1f466b7d16797e64f8207642b016
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-14 14:10:18 +0200

    Cleaned up for clang-tidy

--- sources/gfx/BoidRenderer.cpp
+++ sources/gfx/BoidRenderer.cpp
@@ -7,29 +7,29 @@ constexpr size_t DIRECTIONAL_LIGHT = 1;
 constexpr size_t PLAYER_LIGHT = 2;
 
 BoidRenderer::BoidRenderer(TotoGL::Window& window, TotoGL::Renderer& renderer)
-    : renderer(renderer) {
+    : renderer(renderer)
+    , skydome_texture(TotoGL::TextureFactory::create(
+          TotoGL::Texture(std::ifstream("assets/textures/skydome.jpg"))))
+    , bound_mesh(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/aquarium.obj")))
+    , boid_mesh_high(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/rubio/high.obj")))
+    , boid_mesh_low(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/rubio/low.obj")))
+    , player_mesh(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/goldie.obj")))
+    , lights({ //
+          TotoGL::LightFactory::create(TotoGL::Light({ 1, 1, 1 }, .3, TotoGL::LightType::AMBIENT)),
+          TotoGL::LightFactory::create(TotoGL::Light({ 1, 1, 1 }, 1, TotoGL::LightType::DIRECTIONAL)),
+          TotoGL::LightFactory::create(TotoGL::Light({ 1, .6, 0 }, 3, TotoGL::LightType::POINT)) })
+    , skydome(
+          TotoGL::SkydomeFactory::create(TotoGL::Skydome(*skydome_texture))) {
 
     auto [width, height] = window.size();
     if (width == 0 || height == 0) {
         width = WIDTH;
         height = HEIGHT;
     }
-
-    skydome_texture = TotoGL::TextureFactory::create(
-        TotoGL::Texture(std::ifstream("assets/textures/skydome.jpg")));
-    bound_mesh = TotoGL::MaterialObjectFactory::create(
-        TotoGL::loadWavefront("assets/models/aquarium.obj"));
-    boid_mesh_high = TotoGL::MaterialObjectFactory::create(
-        TotoGL::loadWavefront("assets/models/rubio/high.obj"));
-    boid_mesh_low = TotoGL::MaterialObjectFactory::create(
-        TotoGL::loadWavefront("assets/models/rubio/low.obj"));
-    player_mesh = TotoGL::MaterialObjectFactory::create(
-        TotoGL::loadWavefront("assets/models/goldie.obj"));
-
-    lights.push_back(TotoGL::LightFactory::create(TotoGL::Light({ 1, 1, 1 }, .3, TotoGL::LightType::AMBIENT)));
-    lights.push_back(TotoGL::LightFactory::create(TotoGL::Light({ 1, 1, 1 }, 1, TotoGL::LightType::DIRECTIONAL)));
-    lights.push_back(TotoGL::LightFactory::create(TotoGL::Light({ 1, .6, 0 }, 3, TotoGL::LightType::POINT)));
-    skydome = TotoGL::SkydomeFactory::create(TotoGL::Skydome(*skydome_texture));
 
     boid_mesh_low->scaling() = glm::vec3(.15);
     boid_mesh_high->scaling() = glm::vec3(.15);
@@ -52,7 +52,7 @@ BoidRenderer::~BoidRenderer() {
 // even though the custom one is supposed to skip a lot of boids overhead...
 void BoidRenderer::render(const BoidContainer& container, const Player& player, TotoGL::Camera& used_camera) {
     // auto& used_camera = camera_opt.has_value() ? camera_opt.value() : this->camera;
-    bound_mesh->scaling() = glm::vec3(container.cubeRadius() + .15);
+    bound_mesh->scaling() = glm::vec3(static_cast<float>(container.cubeRadius() + .15));
     player_mesh->position() = player.position();
     lights[PLAYER_LIGHT]->position() = player.position();
     if (glm::abs(player.direction().y) > .99) {
@@ -66,7 +66,7 @@ void BoidRenderer::render(const BoidContainer& container, const Player& player, 
     renderer.clear(false, true, false);
     renderer.render(*bound_mesh, used_camera, lights);
     renderer.render(*player_mesh, used_camera, lights);
-    for (auto& boid : container.boids()) {
+    for (const auto& boid : container.boids()) {
         if (glm::distance(boid.position(), used_camera.position()) < 5) {
             boid_mesh_high->position() = boid.position();
             boid_mesh_high->lookAt(boid.position() + boid.velocity());
