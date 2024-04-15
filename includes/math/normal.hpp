07f2acb0ab85f2aec6baa0526a8a32bf6806fc6c
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-15 18:37:33 +0200

    Added Gaussian parameters

--- includes/math/normal.hpp
+++ includes/math/normal.hpp
@@ -10,20 +10,26 @@ namespace Random {
 template <typename Type>
 class Normal : BaseRng<Type> {
 public:
+    explicit Normal(Type mean = 0, Type standard_deviation = 1)
+        : _mean(mean)
+        , _standard_deviation(standard_deviation) { }
+
     Type operator()() {
         Type u1 = _rng_1();
         Type u2 = _rng_2();
-        return sqrt(-2 * log(u1)) * cos(2 * M_PI * u2);
+        return _standard_deviation * sqrt(-2 * log(u1)) * cos(2 * M_PI * u2) + _mean;
     }
 
     std::pair<Type, Type> generatePair() {
         Type u1 = _rng_1();
         Type u2 = _rng_2();
-        Type r = sqrt(-2 * log(u1));
-        return { r * cos(2 * M_PI * u2), r * sin(2 * M_PI * u2) };
+        Type r = _standard_deviation * sqrt(-2 * log(u1));
+        return { r * cos(2 * M_PI * u2) + _mean, r * sin(2 * M_PI * u2) + _mean };
     }
 
 private:
+    Type _mean { 0 };
+    Type _standard_deviation { 1 };
     Uniform<Type> _rng_1 { 0, 1 };
     Uniform<Type> _rng_2 { 0, 1 };
 };
