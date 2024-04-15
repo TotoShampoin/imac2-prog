bd1763fa135d643ba606d6d67cb2e308ed3ae786
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-15 17:27:36 +0200

    Added.... things

--- includes/prog/BoidSpawner.hpp
+++ includes/prog/BoidSpawner.hpp
@@ -2,6 +2,7 @@
 
 #include "TotoGL/Primitives/Color.hpp"
 #include "math/enumerated.hpp"
+#include "math/normal.hpp"
 #include "math/uniform.hpp"
 #include "prog/Boid.hpp"
 #include <glm/geometric.hpp>
@@ -23,6 +24,7 @@ public:
         boid.velocity() = _randomBox() * _speed;
         boid.setParameters(_boid_force_parameters);
         boid.color() = _color_generator();
+        boid.influence() = _randomSquare();
     }
 
     BoidForceParameters& boidForceParameters() { return _boid_force_parameters; }
@@ -33,20 +35,29 @@ public:
     [[nodiscard]] float speed() const { return _speed; }
 
 private:
+    glm::vec2 _randomSquare() {
+        return { _uniform_generator(), _uniform_generator() };
+    }
     glm::vec3 _randomBox() {
-        return { _coordinate_generator(), _coordinate_generator(), _coordinate_generator() };
+        return { _uniform_generator(), _uniform_generator(), _uniform_generator() };
+    }
+    glm::vec3 _randomSphere() {
+        return glm::normalize(glm::vec3 {
+            _gaussian_generator(),
+            _gaussian_generator(),
+            _gaussian_generator() });
     }
 
     glm::vec3 _position { 0 };
     glm::vec3 _direction { 0, 0, 1 };
-    float _radius { .25 };
+    float _radius { .05 };
     float _speed { 1 };
     BoidForceParameters _boid_force_parameters {
         .avoid = { .force = .2, .zone_width = .5, .zone_offset = 0 },
         .match = { .force = .3, .zone_width = .5, .zone_offset = .5 },
         .center = { .force = .01, .zone_width = .5, .zone_offset = .5 }
     };
-    Random::Uniform<float> _coordinate_generator { -1, 1 };
+    Random::Uniform<float> _uniform_generator { -1, 1 };
     Random::Enumerated<TotoGL::ColorRGB> _color_generator { {
         { .5f, YELLOW },
         { .1f, GREEN },
@@ -55,4 +66,5 @@ private:
         { .1f, MAGENTA },
         { .1f, CYAN },
     } };
+    Random::Normal<float> _gaussian_generator;
 };
