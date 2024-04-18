7c45f96380c342418590eb39e6d8c52042b05b74
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-18 13:06:32 +0200

    refactor

--- includes/gfx/BoidRenderer.hpp
+++ includes/gfx/BoidRenderer.hpp
@@ -20,85 +20,30 @@ constexpr auto FAR = 100.;
 constexpr auto FORCE_FIELD_SCROLL_PERIOD = 10.f;
 
 struct RendererObjects {
-    ~RendererObjects() {
-        for (auto& light : lights) {
-            TotoGL::LightFactory::destroy(light);
-        }
-        TotoGL::SkydomeFactory::destroy(skydome);
-        TotoGL::TextureFactory::destroy(skydome_texture);
-        TotoGL::TextureFactory::destroy(cube_texture);
-        TotoGL::TextureFactory::destroy(cube_blend_texture);
-        TotoGL::RenderObjectFactory::destroy(cube_mesh);
-        TotoGL::MaterialObjectFactory::destroy(boid_mesh_high);
-        TotoGL::MaterialObjectFactory::destroy(boid_mesh_low);
-        TotoGL::MaterialObjectFactory::destroy(player_mesh);
-        TotoGL::MaterialObjectFactory::destroy(bait_mesh);
-        for (auto& world_mesh : world_meshes) {
-            TotoGL::MaterialObjectFactory::destroy(world_mesh);
-        }
-    }
-    RendererObjects() = default;
+    ~RendererObjects();
+    RendererObjects();
     RendererObjects(const RendererObjects&) = delete;
     RendererObjects(RendererObjects&&) = delete;
     RendererObjects& operator=(const RendererObjects&) = delete;
     RendererObjects& operator=(RendererObjects&&) = delete;
 
-    std::vector<TotoGL::LightInstanceId> lights {
-        TotoGL::LightFactory::create(TotoGL::Light({ .25, .5, 1 }, .1, TotoGL::LightType::AMBIENT)),
-        TotoGL::LightFactory::create(TotoGL::Light({ 1, 1, 1 }, 1, TotoGL::LightType::DIRECTIONAL)),
-        TotoGL::LightFactory::create(TotoGL::Light({ 1, .6, 0 }, 3, TotoGL::LightType::POINT))
-    };
+    std::vector<TotoGL::LightInstanceId> lights;
 
-    TotoGL::TextureInstanceId skydome_texture {
-        TotoGL::TextureFactory::create(TotoGL::Texture(std::ifstream("assets/textures/skydome.jpg")))
-    };
-    TotoGL::SkydomeInstanceId skydome {
-        TotoGL::SkydomeFactory::create(TotoGL::Skydome(*skydome_texture))
-    };
+    TotoGL::TextureInstanceId skydome_texture;
+    TotoGL::SkydomeInstanceId skydome;
 
-    TotoGL::TextureInstanceId cube_texture {
-        TotoGL::TextureFactory::create(TotoGL::Texture(std::ifstream("assets/textures/noise.jpg")))
-    };
-    TotoGL::TextureInstanceId cube_blend_texture {
-        TotoGL::TextureFactory::create(TotoGL::Texture(std::ifstream("assets/textures/noise_blend.png")))
-    };
-    TotoGL::RenderObjectInstanceId cube_mesh {
-        TotoGL::RenderObjectFactory::create(
-            TotoGL::RenderObject(
-                TotoGL::MeshFactory::create(TotoGL::Mesh::cube()),
-                TotoGL::ShaderMaterialFactory::create(
-                    TotoGL::ShaderMaterial(
-                        std::ifstream("assets/shaders/shader.vert"),
-                        std::ifstream("assets/shaders/cube.frag")))))
-    };
+    TotoGL::TextureInstanceId cube_texture;
+    TotoGL::TextureInstanceId cube_blend_texture;
+    TotoGL::RenderObjectInstanceId cube_mesh;
 
-    TotoGL::MaterialObjectInstanceId boid_mesh_high {
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/rubio/high.obj"))
-    };
-    TotoGL::MaterialObjectInstanceId boid_mesh_low {
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/rubio/low.obj"))
-    };
+    TotoGL::MaterialObjectInstanceId boid_mesh_high;
+    TotoGL::MaterialObjectInstanceId boid_mesh_low;
 
-    TotoGL::MaterialObjectInstanceId player_mesh {
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/boyo/boyo.obj"))
-    };
+    TotoGL::MaterialObjectInstanceId player_mesh;
 
-    TotoGL::MaterialObjectInstanceId bait_mesh {
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/gem.obj"))
-    };
+    TotoGL::MaterialObjectInstanceId bait_mesh;
 
-    std::vector<TotoGL::MaterialObjectInstanceId> world_meshes {
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/planet/planet.obj")),
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/planet/planet2.obj")),
-        TotoGL::MaterialObjectFactory::create(
-            TotoGL::loadWavefront("assets/models/planet/rock.obj")),
-    };
+    std::vector<TotoGL::MaterialObjectInstanceId> world_meshes;
 };
 
 struct BoidRenderer {
---
+++ sources/gfx/BoidRenderer_assets.cpp
@@ -0,0 +1,67 @@
+#include "gfx/BoidRenderer.hpp"
+
+RendererObjects::RendererObjects()
+    : lights({
+        TotoGL::LightFactory::create(
+            TotoGL::Light({ .25, .5, 1 }, .1, TotoGL::LightType::AMBIENT)),
+        TotoGL::LightFactory::create(
+            TotoGL::Light({ 1, 1, 1 }, 1, TotoGL::LightType::DIRECTIONAL)),
+        TotoGL::LightFactory::create(
+            TotoGL::Light({ 1, .6, 0 }, 3, TotoGL::LightType::POINT)),
+    })
+
+    , skydome_texture(TotoGL::TextureFactory::create(
+          TotoGL::Texture(std::ifstream("assets/textures/skydome.jpg"))))
+    , skydome(TotoGL::SkydomeFactory::create(
+          TotoGL::Skydome(*skydome_texture)))
+
+    , cube_texture(TotoGL::TextureFactory::create(
+          TotoGL::Texture(std::ifstream("assets/textures/noise.jpg"))))
+    , cube_blend_texture(TotoGL::TextureFactory::create(
+          TotoGL::Texture(std::ifstream("assets/textures/noise_blend.png"))))
+    , cube_mesh(TotoGL::RenderObjectFactory::create(
+          TotoGL::RenderObject(
+              TotoGL::MeshFactory::create(
+                  TotoGL::Mesh::cube()),
+              TotoGL::ShaderMaterialFactory::create(
+                  TotoGL::ShaderMaterial(
+                      std::ifstream("assets/shaders/shader.vert"),
+                      std::ifstream("assets/shaders/cube.frag"))))))
+
+    , boid_mesh_high(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/rubio/high.obj")))
+    , boid_mesh_low(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/rubio/low.obj")))
+
+    , player_mesh(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/boyo/boyo.obj")))
+
+    , bait_mesh(TotoGL::MaterialObjectFactory::create(
+          TotoGL::loadWavefront("assets/models/gem.obj")))
+
+    , world_meshes({
+          TotoGL::MaterialObjectFactory::create(
+              TotoGL::loadWavefront("assets/models/planet/planet.obj")),
+          TotoGL::MaterialObjectFactory::create(
+              TotoGL::loadWavefront("assets/models/planet/planet2.obj")),
+          TotoGL::MaterialObjectFactory::create(
+              TotoGL::loadWavefront("assets/models/planet/rock.obj")),
+      }) { }
+
+RendererObjects::~RendererObjects() {
+    for (auto& light : lights) {
+        TotoGL::LightFactory::destroy(light);
+    }
+    TotoGL::SkydomeFactory::destroy(skydome);
+    TotoGL::TextureFactory::destroy(skydome_texture);
+    TotoGL::TextureFactory::destroy(cube_texture);
+    TotoGL::TextureFactory::destroy(cube_blend_texture);
+    TotoGL::RenderObjectFactory::destroy(cube_mesh);
+    TotoGL::MaterialObjectFactory::destroy(boid_mesh_high);
+    TotoGL::MaterialObjectFactory::destroy(boid_mesh_low);
+    TotoGL::MaterialObjectFactory::destroy(player_mesh);
+    TotoGL::MaterialObjectFactory::destroy(bait_mesh);
+    for (auto& world_mesh : world_meshes) {
+        TotoGL::MaterialObjectFactory::destroy(world_mesh);
+    }
+}
