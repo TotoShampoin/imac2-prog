ff65cf3b614f0708d9da31e3e436e6fe79de0854
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-18 22:03:16 +0200

    Refactored histogram

---
+++ includes/math/histogram.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <functional>
+#include <limits>
+#include <vector>
+
+template <typename T>
+struct ProbabilityHistogram {
+    std::vector<float> histogram;
+    float max_count { 0 };
+    float min_value { 0 };
+    float max_value { 0 };
+    size_t HISTOGRAM_SIZE;
+
+    explicit ProbabilityHistogram(
+        const size_t& size,
+        const std::vector<T>& values,
+        const std::function<float(const T&)>& value_getter)
+        : histogram(size, 0)
+        , HISTOGRAM_SIZE(size) {
+        recalculate(values, value_getter);
+    }
+
+    void recalculate(const std::vector<T>& values, const std::function<float(const T&)>& value_getter) {
+        max_count = 0;
+        min_value = 0;
+        max_value = 0;
+        std::fill(histogram.begin(), histogram.end(), 0);
+        if (!values.empty()) {
+            max_count = 0;
+            min_value = std::numeric_limits<float>::max();
+            max_value = std::numeric_limits<float>::min();
+            for (const auto& value : values) {
+                auto v = value_getter(value);
+                min_value = std::min(min_value, v);
+                max_value = std::max(max_value, v);
+            }
+            for (const auto& value : values) {
+                auto v = value_getter(value);
+                auto index = static_cast<size_t>((v - min_value) / (max_value - min_value) * (HISTOGRAM_SIZE - 1));
+                histogram[index] += 1;
+                max_count = std::max(max_count, histogram[index]);
+            }
+        }
+    }
+};
--- sources/gfx/Gui.cpp
+++ sources/gfx/Gui.cpp
@@ -1,5 +1,6 @@
 #include "gfx/Gui.hpp"
 #include "gfx/BoidRenderer.hpp"
+#include "math/histogram.hpp"
 #include "math/variables.hpp"
 #include "prog/BoidContainer.hpp"
 #include "prog/imgui-impl.hpp"
@@ -139,46 +140,16 @@ void UiRenderer::drawStatistics(UiVariables& ui_variables, BoidContainer& contai
     ImGui::End();
 }
 
-template <typename T>
-struct ProbabilityHistogram {
-    std::vector<float> histogram;
-    float max_count { 0 };
-    float min_value { 0 };
-    float max_value { 0 };
-    size_t HISTOGRAM_SIZE;
-
-    explicit ProbabilityHistogram(
-        const size_t& size,
-        const std::vector<T>& values,
-        const std::function<float(const T&)>& value_getter)
-        : histogram(size, 0)
-        , HISTOGRAM_SIZE(size) {
-        recalculate(values, value_getter);
+void UiRenderer::drawStatisticsBoidColors(UiVariables&, BoidContainer& container) {
+    auto colors = Variables::instance()._boid_color_generator.values();
+    for (auto& [color, _] : colors) {
+        size_t count = std::count_if(container.boids().begin(), container.boids().end(), [&](const auto& boid) {
+            return boid.color() == color;
+        });
+        std::string color_code = std::format("#{:02X}{:02X}{:02X}", static_cast<int>(255 * color.r), static_cast<int>(255 * color.g), static_cast<int>(255 * color.b));
+        ImGui::SliderInt(color_code.c_str(), reinterpret_cast<int*>(&count), 0, static_cast<int>(container.boids().size()), "%d", ImGuiSliderFlags_NoInput);
     }
-
-    void recalculate(const std::vector<T>& values, const std::function<float(const T&)>& value_getter) {
-        max_count = 0;
-        min_value = 0;
-        max_value = 0;
-        std::fill(histogram.begin(), histogram.end(), 0);
-        if (!values.empty()) {
-            max_count = 0;
-            min_value = std::numeric_limits<float>::max();
-            max_value = std::numeric_limits<float>::min();
-            for (const auto& value : values) {
-                auto v = value_getter(value);
-                min_value = std::min(min_value, v);
-                max_value = std::max(max_value, v);
-            }
-            for (const auto& value : values) {
-                auto v = value_getter(value);
-                auto index = static_cast<size_t>((v - min_value) / (max_value - min_value) * (HISTOGRAM_SIZE - 1));
-                histogram[index] += 1;
-                max_count = std::max(max_count, histogram[index]);
-            }
-        }
-    }
-};
+}
 
 // TODO(Stats) Get the data from the spawner, not from the simulation itself
 void UiRenderer::drawStatisticsBoidForces(UiVariables&, BoidContainer& container, BoidSpawner& spawner) {
@@ -246,17 +217,6 @@ void UiRenderer::drawStatisticsBoidForces(UiVariables&, BoidContainer& container
     ImGui::Text("%f - %f", boid_histogram.min_value, boid_histogram.max_value);
     ImGui::Text("Mean: %f", _strength_generator.mean());
     ImGui::Text("Standard deviation: %f", _strength_generator.standardDeviation());
-}
-
-void UiRenderer::drawStatisticsBoidColors(UiVariables&, BoidContainer& container) {
-    auto colors = Variables::instance()._boid_color_generator.values();
-    for (auto& [color, _] : colors) {
-        size_t count = std::count_if(container.boids().begin(), container.boids().end(), [&](const auto& boid) {
-            return boid.color() == color;
-        });
-        std::string color_code = std::format("#{:02X}{:02X}{:02X}", static_cast<int>(255 * color.r), static_cast<int>(255 * color.g), static_cast<int>(255 * color.b));
-        ImGui::SliderInt(color_code.c_str(), reinterpret_cast<int*>(&count), 0, static_cast<int>(container.boids().size()), "%d", ImGuiSliderFlags_NoInput);
-    }
 }
 
 void UiRenderer::drawStatisticsPlanetSpawning(UiVariables& ui_variables, BoidRenderer& boid_renderer) {
