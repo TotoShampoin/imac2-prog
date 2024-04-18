d18e9837a3b1f4edb9412ec18a35b3de0aa1bf2e
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-18 11:46:35 +0200

    Added second order dynamics to player

--- includes/gfx/Player.hpp
+++ includes/gfx/Player.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "TotoGL/Primitives/Clock.hpp"
+#include "gfx/SecondOrderDynamics.hpp"
 #include <TotoGL/TotoGL.hpp>
 #include <glm/fwd.hpp>
 
@@ -8,13 +9,17 @@ class Player {
 public:
     glm::vec3& position() { return _position; }
     glm::vec3& direction() { return _direction; }
+    glm::vec3& frontDirection() { return _front_direction; }
     glm::vec3& velocity() { return _velocity; }
     glm::vec3& push() { return _push; }
     float& speed() { return _speed; }
     float& pushForce() { return _push_force; }
 
+    [[nodiscard]] glm::vec3 direction() const { return *_target_dynamic; }
+
     [[nodiscard]] glm::vec3 position() const { return _position; }
-    [[nodiscard]] glm::vec3 direction() const { return _direction; }
+    [[nodiscard]] glm::vec3 trueDirection() const { return _direction; }
+    [[nodiscard]] glm::vec3 frontDirection() const { return _front_direction; }
     [[nodiscard]] glm::vec3 velocity() const { return _velocity; }
     [[nodiscard]] glm::vec3 push() const { return _push; }
     [[nodiscard]] float speed() const { return _speed; }
@@ -28,8 +33,11 @@ public:
 private:
     glm::vec3 _position { 0 };
     glm::vec3 _direction { 0, 0, -1 };
+    glm::vec3 _front_direction { 0, 0, -1 };
     glm::vec3 _velocity { 0 };
     glm::vec3 _push { 0 };
     float _speed { 10.f };
     float _push_force { 30.f };
+
+    SecondOrderDynamics<glm::vec3> _target_dynamic { { 1, 1, 0 }, _direction };
 };
--- sources/gfx/Player.cpp
+++ sources/gfx/Player.cpp
@@ -2,11 +2,16 @@
 
 void Player::update(const TotoGL::Seconds& delta) {
     _position += delta * (_velocity + _push);
+    _target_dynamic.update(delta, _direction);
 }
 
 void Player::move(const glm::vec3& direction, const TotoGL::Seconds& delta) {
-    if (glm::length(direction) > 0)
+    if (glm::length(direction) > 0) {
         _direction = glm::normalize(direction);
+        if (glm::abs(_direction.y) < .99) {
+            _front_direction = glm::normalize(glm::vec3(_direction.x, 0, _direction.z));
+        }
+    }
     _velocity = glm::mix(_velocity, direction * _speed, delta);
 }
 
