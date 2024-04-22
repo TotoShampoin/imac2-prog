b463119203683b542707e9e233cf10fcf4e7c07e
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-22 18:22:31 +0200

    added expected and variance

--- includes/math/histogram.hpp
+++ includes/math/histogram.hpp
@@ -11,6 +11,8 @@ struct ProbabilityHistogram {
     float min_value { 0 };
     float max_value { 0 };
     size_t HISTOGRAM_SIZE;
+    float expected_value { 0 };
+    float variance { 0 };
 
     explicit ProbabilityHistogram(
         const size_t& size,
@@ -30,11 +32,18 @@ struct ProbabilityHistogram {
             max_count = 0;
             min_value = std::numeric_limits<float>::max();
             max_value = std::numeric_limits<float>::min();
+            expected_value = 0;
+            float expected_value_squared = 0;
+            variance = 0;
             for (const auto& value : values) {
                 auto v = value_getter(value);
                 min_value = std::min(min_value, v);
                 max_value = std::max(max_value, v);
+                expected_value += v;
+                expected_value_squared += v * v;
             }
+            expected_value /= values.size();
+            variance = expected_value_squared / values.size() - expected_value * expected_value;
             for (const auto& value : values) {
                 auto v = value_getter(value);
                 auto index = static_cast<size_t>((v - min_value) / (max_value - min_value) * (HISTOGRAM_SIZE - 1));
